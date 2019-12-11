e3ccc0e80bbc57d7755f6e973da6c88e5794c724
Keishiro Nishiyama <kn11308@gmail.com>
2019-12-11 10:57:55 +0900

    implement component with connnect function

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -6,10 +6,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return { value: state.value + 1 }
-      break;
     case DECREMENT:
-      return { vlue: state.value - 1 }
-      break;
+      return { value: state.value - 1 }
     default:
       return state
   }
