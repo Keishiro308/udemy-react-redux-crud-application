dcfacd865859a49ec1d2891818d917642618a2dd
Keishiro Nishiyama <kn11308@gmail.com>
2019-12-12 23:08:38 +0900

    apply material-ui to event_new and events_show

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,9 @@ import React, {Component}  from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 import { postEvent } from '../actions';
 
@@ -13,10 +16,15 @@ class EventsNew extends Component {
   renderField(field){
     const { input, label, type, meta:{ touched, error} } = field
     return (
-      <div>
-        <input {...input} placeholder={label} type={type}/>
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hinttext={label}
+        label={label}
+        type={type}
+        error={!!(touched && error)}
+        helperText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -27,17 +35,24 @@ class EventsNew extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
-    console.log(submitting)
+    const style={
+      margin: 12
+    }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
-        <div><Field label='Title' name='title' type='text' component={this.renderField}/></div>
+
+        <div>
+          <Field label='Title' name='title' type='text' component={this.renderField} />
+        </div>
         <div>
           <Field label='Body' name='body' type='text' component={this.renderField}/>
         </div>
-        <div>
-          <input type='submit' value='Submit' disabled={ pristine || submitting || invalid }/>
-          <Link to="/" >Cancel</Link>
-        </div>
+        <Button type="submit" style={style} disabled= {pristine || submitting || invalid } >
+          Submit
+        </Button>
+        <Button  style={style} href='/'>
+          Cancel
+        </Button>
       </form>
     )
   }
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, {Component}  from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import { getEvent, deleteEvent, putEvent } from '../actions';
 
@@ -18,10 +20,15 @@ class EventsShow extends Component {
   renderField(field){
     const { input, label, type, meta:{ touched, error} } = field
     return (
-      <div>
-        <input {...input} placeholder={label} type={type}/>
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hinttext={label}
+        label={label}
+        type={type}
+        error={!!(touched && error)}
+        helperText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -37,7 +44,9 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
-    console.log(submitting)
+    const style={
+      margin: 12
+    }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label='Title' name='title' type='text' component={this.renderField}/></div>
@@ -45,9 +54,15 @@ class EventsShow extends Component {
           <Field label='Body' name='body' type='text' component={this.renderField}/>
         </div>
         <div>
-          <input type='submit' value='Submit' disabled={ pristine || submitting || invalid }/>
-          <Link to="/" >Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
+          <Button type="submit" style={style} disabled= {pristine || submitting || invalid } >
+            Submit
+          </Button>
+          <Button  style={style} href='/'>
+            Cancel
+          </Button>
+          <Button  style={style} onClick={this.onDeleteClick}>
+            Delete
+          </Button>
         </div>
       </form>
     )
